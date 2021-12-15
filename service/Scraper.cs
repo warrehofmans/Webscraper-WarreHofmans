1d0d4a3df1cca95d8e806f03900f45c4bb9519cd
WarreH <warrehofmans@gmail.com>
2021-12-15 10:07:13 +0100

    option 4

--- service/Scraper.cs
+++ service/Scraper.cs
@@ -39,7 +39,7 @@ namespace Webscraper_ConsoleApplication
             var path = (Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)) + "\\driver";
             //driver = new ChromeDriver(@path, capabilities);
             //driver = new ChromeDriver(@path, capabilities);
-            driver = new ChromeDriver(Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory), capabilities);
+            driver = new ChromeDriver(".",capabilities);
 
         
 
